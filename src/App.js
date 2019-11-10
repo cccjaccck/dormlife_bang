02e865f075ba777e868bf5264e123c3a97119237
cccjaccck <vynx2006@gmail.com>
2019-11-10 22:42:22 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import Memo from './components/Memo';
+import Profile from './components/Profile';
 import './App.css';
 
+const data = {
+  room: {
+    profiles: [
+      {
+        photo: "https://source.unsplash.com/random/50x50/",
+        room: "A방",
+        name: "Larry"
+      },
+      {
+        photo: "https://source.unsplash.com/random/50x50.1/",
+        room: "B방",
+        name: "간지Jack"
+      },
+      {
+        photo: "https://source.unsplash.com/random/50x50.2/",
+        room: "C방",
+        name: "Tom슨121"
+      }
+    ],
+    memo: {
+      title: "memo",
+      content: "오늘은 공용 비품의 날!\n공용 화장지 제출해주세요~"
+    },
+    tip: {
+      title: "tip",
+      content: "오늘의 꿀팁!"
+    }
+  }
+};
+
 function App() {
+  const {
+      room: {
+        profiles,
+        memo,
+        tip
+      }
+    }    
+   = data;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {
+        profiles.map(profile => (
+          <Profile
+          photo={profile.photo}
+          room={profile.room}
+          name={profile.name}
+          />
+        ))
+      }
+      <Memo 
+        title={memo.title}
+        content={memo.content}/>
+      <Memo 
+        title={tip.title}
+        content={tip.content}/>
     </div>
   );
 }
